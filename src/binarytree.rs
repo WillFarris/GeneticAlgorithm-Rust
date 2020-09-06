363b1a2f6c6a1c58199c0668137b8518f7714cdf
Will Farris <9190155+GetLiquid@users.noreply.github.com>
2020-09-06 15:05:51 -0700

    add beginning of expression tree

---
+++ src/binarytree.rs
@@ -0,0 +1,42 @@
+/// Binary Tree implementation using traits
+/// 
+/// A binary tree holds a reference to some data and two child nodes, which can be `None` or
+/// the root of a subtree.
+pub struct BinaryTree<'a, T> {
+    pub val: &'a T,
+    pub left: Option<Box<BinaryTree<'a, T>>>,
+    pub right: Option<Box<BinaryTree<'a, T>>>,
+}
+
+impl<'a, T: PartialOrd + std::fmt::Debug> BinaryTree<'a, T> {
+    pub fn new(new_val: &'a T) -> BinaryTree<T> {
+        BinaryTree {
+            val: new_val,
+            left: None,
+            right: None,
+        }
+    }
+
+    pub fn insert(&mut self, new_val: &'a T) {
+        let target = if new_val < self.val {&mut self.left} else {&mut self.right};
+        match target {
+            &mut Some(ref mut subtree) => subtree.insert(new_val),
+            &mut None => {
+                let new_binary_tree = BinaryTree::new(new_val);
+                let boxed_binary_tree = Some(Box::new(new_binary_tree));
+                *target = boxed_binary_tree;
+            }
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn print(&self, level: u64) {
+        println!("level {}: {:?}", level, self.val);
+        if self.left.is_some() {
+            self.left.as_ref().unwrap().print(level + 1);
+        }
+        if self.right.is_some() {
+            self.right.as_ref().unwrap().print(level + 1);
+        }
+    }
+}
--- src/genome.rs
+++ src/genome.rs
@@ -1,16 +1,18 @@
+use crate::binarytree::BinaryTree;
+
 pub struct Genome {
-    gene: u64,
-    fitness: f64,
+    pub gene: u64,
+    pub fitness: f64,
     pub sequenced: Option<String>,
 }
 
 impl Genome {
     pub fn new() -> Genome {
-        let new_gene = rand::random::<u64>();
+        let gene = rand::random::<u64>();
         let fitness = 0.0;
         Genome {
-            gene: new_gene,
-            fitness: fitness,
+            gene,
+            fitness,
             sequenced: None,
         }
     }
@@ -19,19 +21,81 @@ impl Genome {
         if self.sequenced != None {
             return
         }
-
-        let mut temp: String = String::with_capacity(16);
+        
+        let mut temp = String::with_capacity(16);
+        
+        let mut prev_raw = 0xF;
         for i in 0..16 {
-            let cur = ((self.gene >> i*4) & 0xF) as u8;
-            match cur {
-                    0xA => temp.push('+'),
-                    0xB => temp.push('-'),
-                    0xC => temp.push('*'),
-                    0xD => temp.push('/'),
-                    0xE..=0xF => (),
-                    _   => temp.push((cur + 48) as char), //48 == ascii 0
+            let cur_raw = ((self.gene >> i*4) & 0xF) as u8;
+            
+            if (cur_raw < 0xA && prev_raw > 0x9) || (cur_raw > 0x9 && prev_raw < 0xA) && cur_raw < 0xE {
+                let cur_char = match cur_raw {
+                    0xA => '*',
+                    0xB => '/',
+                    0xC => '+',
+                    0xD => '-',
+                    0xE..=0xF => '#', // ignored
+                    _   => (cur_raw + 48) as char, //48 == ascii '0'
+                };
+                temp.push(cur_char);
+                prev_raw = cur_raw;
             }
         }
+
+        /*let mut last_was_op = true;
+        let mut last = 0xF;
+        let mut index = 0;
+        while index < 16 {
+            let mut cur = (self.gene >> i*4) & 0xF;
+            let is_op = cur > 9;
+            while last_was_op && cur > 9 {
+                index += 1;
+                cur = (self.gene >> i*4) & 0xF;
+            }
+            last_was_op = false;
+            last = cur;
+            let cur_char = match cur_raw {
+                0xA => '*',
+                0xB => '/',
+                0xC => '+',
+                0xD => '-',
+                0xE..=0xF => '#', // ignored
+                _   => (cur_raw + 48) as char, //48 == ascii '0'
+            };
+            temp.push(cur_char);
+        }*/
+
         self.sequenced = Some(temp);
     }
+
+    pub fn evaulate(&self) -> f64 {
+        let mut members: [u8; 16] = [0xF; 16];
+        for i in 0..16 {
+            let current = ((self.gene >> (i*4)) & 0xF) as u8;
+            members[i] = current;
+        }
+        println!("{:?}", members);
+        //let root = build_tree(&members).unwrap();
+        //root.print(0);
+        0.0
+    }
+}
+
+#[allow(dead_code)]
+fn build_tree(subarray: &[u8]) -> Option<Box<BinaryTree<u8>>> {
+    if subarray.len() <= 0 {
+        return None
+    }
+    let mut last_op_index = 0;
+    for i in subarray.len()..=0 {
+        if subarray[last_op_index] > subarray[i] {
+            last_op_index = i;
+        }
+    }
+
+    Some(Box::new(BinaryTree {
+        val: &subarray[last_op_index],
+        left: build_tree(&subarray[0..last_op_index]),
+        right: build_tree(&subarray[last_op_index..subarray.len()])
+    }))
 }
